5aeec0117244c06ea61f97bf92d4f139f331c8c6
PCBOON <109321174+PCBOON@users.noreply.github.com>
2023-06-19 15:21:00 +0800

    Update music.js

--- js/music.js
+++ js/music.js
@@ -9,9 +9,9 @@ https://api.wuenci.com/meting/api/
 GitHub：https://github.com/imsyy/home
 版权所有，请勿删除
 */
-var server = "tencent"; //netease: 网易云音乐; tencent: QQ音乐; kugou: 酷狗音乐; xiami: 虾米; kuwo: 酷我
+var server = "netease"; //netease: 网易云音乐; tencent: QQ音乐; kugou: 酷狗音乐; xiami: 虾米; kuwo: 酷我
 var type = "playlist"; //song: 单曲; playlist: 歌单; album: 唱片
-var id = "3720456183"; //封面 ID / 单曲 ID / 歌单 ID
+var id = "3058607834"; //封面 ID / 单曲 ID / 歌单 ID
 
 $.ajax({
     url: "https://api.wuenci.com/meting/api/?server=" + server + "&type=" + type + "&id=" + id,
